53f076e5c12897d4353c1ee3d6d9e50a2c1954a7
krish <krischuk0300@gmail.com>
2023-11-15 12:13:46 +0200

    lab3.js ready

--- lab3/lab3.js
+++ lab3/lab3.js
@@ -12,12 +12,14 @@ function makeList() {
     });
 }
 function editCell(cell) {
-    // Перевірка, чи вже є edit-container в комірці
-    if (cell.querySelector('.edit-container')) {
+    var editContainer = cell.querySelector('.edit-container');
+
+    if (editContainer) {
+        editContainer.remove();
         return;
     }
 
-    var editContainer = document.createElement('div');
+    editContainer = document.createElement('div');
     editContainer.className = 'edit-container';
 
     var input = document.createElement('input');
@@ -25,14 +27,14 @@ function editCell(cell) {
 
     var button = document.createElement('button');
     button.innerText = 'OK';
-    button.onclick = function () {
-        cell.textContent = input.value;
-        editContainer.removeChild(input);
-        editContainer.removeChild(button)
-    };
-
     editContainer.appendChild(input);
     editContainer.appendChild(button);
     cell.appendChild(editContainer);
     input.focus();
+    button.onclick = function () {
+        cell.textContent = input.value;
+        editContainer.remove();
+        return;
+    };
+   
 }
